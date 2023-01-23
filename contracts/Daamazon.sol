e68aff507a5f545cd5ea966fe2ea8ecca80d8d94
Marpureddy Saswat <saswatmarpureddy@gmail.com>
2023-01-24 01:06:17 +0530

    Listing

--- contracts/Daamazon.sol
+++ contracts/Daamazon.sol
@@ -4,9 +4,44 @@ pragma solidity ^0.8.9;
 contract Daamazon {
     
     address public owner;
-constructor(){
 
+    struct Item{
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+mapping(uint256 =>Item)public items;
+constructor(){
 owner=msg.sender;
+
+}
+function list (
+uint256 _id,
+string memory _name,
+string memory _category,
+string memory _image,
+uint256 _cost,
+uint256 _rating,
+uint256 _stock)public{
+   
+
+Item memory item=Item(
+_id,
+_name,
+_category,
+_image,
+_cost,
+_rating,
+_stock);
+
+items[_id]=item;
+
+
+
 
 }
 
--- test/Daamazon.js
+++ test/Daamazon.js
@@ -4,6 +4,14 @@ const { ethers } = require("hardhat");
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
 };
+const ID = 1;
+const NAME = "Shoes";
+const CATEGORY = "Clothing";
+const IMAGE =
+  "https://ipfs.io/ipfs/QmTYEboq8raiBs7GTUg2yLXB3PMz6HuBNgNfSZBx5Msztg/shoes.jpg";
+const COST = tokens(1);
+const RATING = 4;
+const STOCK = 5;
 
 describe("Daamazon", () => {
   let daamazon;
@@ -19,6 +27,26 @@ describe("Daamazon", () => {
   describe("Deployment", () => {
     it("Sets the owner", async () => {
       expect(await daamazon.owner()).to.equal(deployer.address);
+    });
+    describe("Listing", () => {
+      let transaction;
+
+      beforeEach(async () => {
+        transaction = await daamazon
+          .connect(deployer)
+          .list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+        await transaction.wait();
+      });
+      it("returns item attributes", async () => {
+        const item = await daamazon.items(ID);
+        expect(item.id).to.equal(1);
+        expect(item.name).to.equal(NAME);
+        expect(item.category).to.equal(CATEGORY);
+        expect(item.image).to.equal(IMAGE);
+        expect(item.cost).to.equal(COST);
+        expect(item.rating).to.equal(RATING);
+        expect(item.stock).to.equal(STOCK);
+      });
     });
   });
 });
