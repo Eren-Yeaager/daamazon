cce0a1a244197d0069452651cbdea017df72e71d
Marpureddy Saswat <saswatmarpureddy@gmail.com>
2023-01-26 22:54:31 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -7,15 +7,49 @@ import Section from "./components/Section";
 import Product from "./components/Product";
 
 // ABIs
-import Dappazon from "./abis/Dappazon.json";
+import Daamazon from "./abis/Daamazon.json";
 
 // Config
 import config from "./config.json";
 
 function App() {
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [daamazon, setDaamazon] = useState(null);
+  const [electronics, setElectronics] = useState(null);
+  const [clothing, setClothing] = useState(null);
+  const [toys, setToys] = useState(null);
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+
+    const network = await provider.getNetwork();
+    const daamazon = new ethers.Contract(
+      config[network.chainId].daamazon.address,
+      Daamazon,
+      provider
+    );
+    setDaamazon(daamazon);
+    const items = [];
+    for (var i = 0; i < 9; i++) {
+      const item = await daamazon.items(i + 1);
+      items.push(item);
+    }
+    const electronics = items.filter((item) => item.category === "electronics");
+    const clothing = items.filter((item) => item.category === "clothing");
+    const toys = items.filter((item) => item.category === "toys");
+  };
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
   return (
     <div>
-      <h2>Welcome to Daamazon</h2>
+      <Navigation account={account} setAccount={setAccount} />
+      <h2>Daamazon Best Sellers</h2>
+     {electronics&&clothing&&toys?App(
+
+      <p>Products</p>
+     )}
     </div>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,43 @@
-import { ethers } from 'ethers';
+import { ethers } from "ethers";
 
 const Navigation = ({ account, setAccount }) => {
-
-    return (
-        <nav>
-
-        </nav>
-    );
-}
+  const connectHandler = async () => {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const account = ethers.utils.getAddress(accounts[0]);
+    setAccount(account);
+    console.log("Connecting...");
+  };
+  return (
+    <nav>
+      <div className="nav__brand">
+        <h1>Daamazon</h1>
+      </div>
+      <input type="text" className="nav__search" />
+      {account ? (
+        <button type="button" className="nav__connect">
+          {account.slice(0, 6) + "..." + account.slice(38, 42)}
+        </button>
+      ) : (
+        <button type="button" className="nav__connect" onClick={connectHandler}>
+          Connect
+        </button>
+      )}
+      <ul className="nav__links">
+        <li>
+          <a href="#Clothing & Jewelry">Clothing & Jewelry</a>
+        </li>
+        <li>
+          <a href="#Electronics &Gadgets">Electronics and Gadgets</a>
+        </li>
+        <li>
+          {" "}
+          <a href="#Toys and Gaming">Toys and Gaming</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navigation;
